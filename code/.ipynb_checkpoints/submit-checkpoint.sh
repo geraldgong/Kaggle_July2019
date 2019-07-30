23c5b78a6e3f361d2b73c1a611ac7fbd8384d434
ygong@sms1.local <ygong@agtinternational.com>
2019-07-30 18:28:07 +0200

    try with lightgb

--- code/.ipynb_checkpoints/submit-checkpoint.sh
+++ code/.ipynb_checkpoints/submit-checkpoint.sh
@@ -1,3 +1,2 @@
-# kaggle competitions submit -c champs-scalar-coupling -f /home/ygong/Documents/Kaggle_July2019/output/submission.csv -m "J-coupling submission" 
-
-kaggle competitions submit -c champs-scalar-coupling -f /home/gong/Documents/Kaggle_July2019/output/submission.csv -m "J-coupling submission" 
+kaggle competitions submit -c champs-scalar-coupling -f /home/ygong/Documents/Kaggle_July2019/output/submission.csv -m "J-coupling submission" 
+# kaggle competitions submit -c champs-scalar-coupling -f /home/gong/Documents/Kaggle_July2019/output/submission.csv -m "J-coupling submission" 
